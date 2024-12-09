81b7fa2d4173df2c5c289b3e3551aa5c5bb6aa45
antonio.coppola <antonio.coppola@tum.de>
2024-12-09 10:25:37 +0100

    refactoring

--- src/solutions/model_solution.py
+++ src/solutions/model_solution.py
@@ -1,53 +1,81 @@
 from input_data import SolverParameters
-from congestion_model.core import get_free_flow_schedule, \
-    get_total_travel_time, get_staggering_applicable
+from congestion_model.core import (
+    get_free_flow_schedule,
+    get_total_travel_time,
+    get_staggering_applicable,
+)
 from instance_module.epoch_instance import EpochInstance
 from utils.classes import EpochSolution
 from gurobipy import Model
 import gurobipy as grb
 
 
-def _get_model_release_times(model: Model, paths: list[list[int]]) -> list[float]:
+def get_model_release_times(model: Model, paths: list[list[int]]) -> list[float]:
+    """Retrieve the release times from the model."""
     return [model._departure[vehicle][path[0]].X for vehicle, path in enumerate(paths)]
 
 
-def _get_model_schedule(model: Model, paths: list[list[int]]) -> list[list[float]]:
+def get_model_schedule(model: Model, paths: list[list[int]]) -> list[list[float]]:
+    """Retrieve the schedule from the model."""
     return [[model._departure[vehicle][arc].X for arc in path] for vehicle, path in enumerate(paths)]
 
 
-def _get_model_delay_on_arcs(model: Model, paths: list[list[int]]) -> list[list[float]]:
-    return [[model._delay[vehicle][arc].X if isinstance(model._delay[vehicle][arc], grb.Var) else 0 for arc in path] for
-            vehicle, path in enumerate(paths)]
+def get_model_delay_on_arcs(model: Model, paths: list[list[int]]) -> list[list[float]]:
+    """Retrieve the delay on arcs from the model."""
+    return [
+        [
+            model._delay[vehicle][arc].X if isinstance(model._delay[vehicle][arc], grb.Var) else 0
+            for arc in path
+        ]
+        for vehicle, path in enumerate(paths)
+    ]
 
 
-def _get_staggering_applied(releaseTimesSolution, releaseTimesStatusQuo):
-    return [releaseTimesSolution[i] - releaseTimesStatusQuo[i] for i in range(len(releaseTimesStatusQuo))]
+def get_staggering_applied(release_times_solution, release_times_status_quo):
+    """Compute the staggering applied."""
+    return [
+        release_times_solution[i] - release_times_status_quo[i]
+        for i in range(len(release_times_status_quo))
+    ]
 
 
-def get_epoch_model_solution(model: Model, epochInstance: EpochInstance, epochStatusQuo: EpochSolution,
-                             epochWarmStart: EpochSolution, solver_params: SolverParameters) -> EpochSolution:
+def get_epoch_model_solution(
+        model: Model,
+        epoch_instance: EpochInstance,
+        epoch_status_quo: EpochSolution,
+        epoch_warm_start: EpochSolution,
+        solver_params: SolverParameters,
+) -> EpochSolution:
+    """
+    Compute the epoch model solution.
+    """
+    # If optimization is disabled, return warm start or status quo
     if not solver_params.optimize or not model._optimize:
         if not solver_params.warm_start:
-            return epochStatusQuo
-        return epochWarmStart
+            return epoch_status_quo
+        return epoch_warm_start
 
-    totalDelay = model._totalDelay.X
-    releaseTimes = _get_model_release_times(model, epochInstance.trip_routes)
-    congestedSchedule = _get_model_schedule(model, epochInstance.trip_routes)
-    delaysOnArcs = _get_model_delay_on_arcs(model, epochInstance.trip_routes)
-    freeFlowSchedule = get_free_flow_schedule(epochInstance, congestedSchedule)
-    staggeringApplied = _get_staggering_applied(releaseTimes, epochStatusQuo.release_times)
-    slack = get_staggering_applicable(epochInstance, staggeringApplied)
-    totalTravelTime = get_total_travel_time(congestedSchedule)
+    # Retrieve results from the optimized model
+    total_delay = model._totalDelay.X
+    release_times = get_model_release_times(model, epoch_instance.trip_routes)
+    congested_schedule = get_model_schedule(model, epoch_instance.trip_routes)
+    delays_on_arcs = get_model_delay_on_arcs(model, epoch_instance.trip_routes)
+    free_flow_schedule = get_free_flow_schedule(epoch_instance, congested_schedule)
+    staggering_applied = get_staggering_applied(release_times, epoch_status_quo.release_times)
+    slack = get_staggering_applicable(epoch_instance, staggering_applied)
+    total_travel_time = get_total_travel_time(congested_schedule)
 
-    modelSolution = EpochSolution(delays_on_arcs=delaysOnArcs,
-                                  free_flow_schedule=freeFlowSchedule,
-                                  release_times=releaseTimes,
-                                  staggering_applicable=slack,
-                                  total_delay=totalDelay,
-                                  congested_schedule=congestedSchedule,
-                                  staggering_applied=staggeringApplied,
-                                  vehicles_utilizing_arcs=epochStatusQuo.vehicles_utilizing_arcs,
-                                  total_travel_time=totalTravelTime)
+    # Construct the solution
+    model_solution = EpochSolution(
+        delays_on_arcs=delays_on_arcs,
+        free_flow_schedule=free_flow_schedule,
+        release_times=release_times,
+        staggering_applicable=slack,
+        total_delay=total_delay,
+        congested_schedule=congested_schedule,
+        staggering_applied=staggering_applied,
+        vehicles_utilizing_arcs=epoch_status_quo.vehicles_utilizing_arcs,
+        total_travel_time=total_travel_time,
+    )
 
-    return modelSolution
+    return model_solution
