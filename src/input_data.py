579e99d683210de8c243a0fb7bfcd574be050b65
antonio.coppola <antonio.coppola@tum.de>
2024-12-23 20:17:01 +0100

    231224_ALGO_PERF_PAPER_MAN100_SHORTYES_MF

--- src/input_data.py
+++ src/input_data.py
@@ -127,12 +127,12 @@ def print_parameters(instance_parameters, solver_parameters):
 
 def generate_input_data_from_script() -> tuple[InstanceParameters, SolverParameters]:
     instance_params = InstanceParameters(
-        day=5, number_of_trips=200, seed=0, network_name="manhattan_10",
+        day=5, number_of_trips=300, seed=0, network_name="manhattan_10",
         max_flow_allowed=100, add_shortcuts=True, list_of_slopes=[0.15], list_of_thresholds=[1],
         deadline_factor=100, staggering_cap=25)
 
     solver_params = SolverParameters(epoch_size=60, algorithm_time_limit=1000, epoch_time_limit=1000,
-                                     optimize=True, warm_start=True, improve_warm_start=True,
+                                     optimize=False, warm_start=True, improve_warm_start=True,
                                      local_search_callback=True,
                                      simplify=True, instance_parameters=instance_params, set_of_experiments="local",
                                      verbose_model=True)
