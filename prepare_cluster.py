9433ff23e2ecb4321b523fd87fcd56141be2e9d3
antonio.coppola <antonio.coppola@tum.de>
2024-12-26 13:24:20 +0100

    261224_VAR_PWL_MAN10_SHORTYES_MF

--- prepare_cluster.py
+++ prepare_cluster.py
@@ -1,8 +1,9 @@
 import csv
 import itertools
 import os
+import pprint
 import shutil
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from pathlib import Path
 from tabulate import tabulate
 import datetime
@@ -66,8 +67,11 @@ def remove_old_setups():
 
 def create_cluster_configuration(cluster_setup: ClusterSetup) -> None:
     """Creates a cluster configuration bash script with the given setup."""
-
-    print(cluster_setup)
+    # Assuming cluster_setup is your dataclass instance
+    print("=" * 50)
+    print("CLUSTER SETUP".center(50))
+    print("=" * 50)
+    pprint.pprint(asdict(cluster_setup))
     with open("my_cluster_configuration.sh", "w") as file:
         file.writelines([
             "#!/bin/bash\n",
@@ -113,29 +117,15 @@ def write_instance_parameters_csv(input_data_dict, input_data_name, mode: str):
 
 
 PRESETS = {
-    "algo_performance": {
-        "day_list": list(range(1, 32)),  # start instance params
-        "max_flow_allowed_list": [10, 100],
+    "var_pwl": {
+        "network_name": "manhattan_10",
+        "number_of_trips": 500,
+        "day_list": list(range(1, 6)),  # start instance params
+        "max_flow_allowed_list": [30, 60],
         "seed_list": [0],
-        "list_of_slopes": [0.15],
-        "list_of_thresholds": [1],
+        "list_of_slopes_list": [[0.15], [0.15, 0.3], [0.15, 0.3, 0.5]],
+        "list_of_thresholds_list": [[1], [1, 1.5], [1, 1.5, 2]],
         "staggering_cap_list": [25],
-        "deadline_factor": 100,  # end instance params
-        "algo_mode_list": ["OFFLINE", "ONLINE"],
-        "optimize": True,
-        "warm_start": True,
-        "improve_warm_start": True,
-        "simplify": True,
-        "local_search_callback": True  # end solver params
-
-    },
-    "staggering_analysis": {
-        "day_list": [1],  # start instance params
-        "max_flow_allowed_list": [10, 100],
-        "seed_list": [0],
-        "list_of_slopes": [0.15],
-        "list_of_thresholds": [1],
-        "staggering_cap_list": [x * 2.5 for x in range(11)],  # Generates [0.0, 2.5, 5.0, ..., 25.0]
         "deadline_factor": 100,  # end instance params
         "algo_mode_list": ["OFFLINE"],
         "optimize": True,
@@ -145,11 +135,13 @@ PRESETS = {
         "local_search_callback": True  # end solver params
     },
     "algo_performance_paper": {
+        "network_name": "manhattan_100",
+        "number_of_trips": 5000,
         "day_list": list(range(1, 32)),  # start instance params
         "max_flow_allowed_list": [20, 40],
         "seed_list": [0],
-        "list_of_slopes": [.5],
-        "list_of_thresholds": [1],
+        "list_of_slopes_list": [[.5]],
+        "list_of_thresholds_list": [[1]],
         "staggering_cap_list": [10],
         "deadline_factor": 25,  # end instance params
         "algo_mode_list": ["OFFLINE", "ONLINE"],
@@ -160,11 +152,13 @@ PRESETS = {
         "local_search_callback": True  # end solver params
     },
     "staggering_analysis_paper": {
+        "network_name": "manhattan_100",
+        "number_of_trips": 5000,
         "day_list": [3],  # start instance params
         "max_flow_allowed_list": [20, 40],
         "seed_list": [0],
-        "list_of_slopes": [.5],
-        "list_of_thresholds": [1],
+        "list_of_slopes_list": [[.5]],
+        "list_of_thresholds_list": [[1]],
         "staggering_cap_list": [x * 2.5 for x in range(11)],  # Generates [0.0, 2.5, 5.0, ..., 25.0]
         "deadline_factor": 100,  # end instance params
         "algo_mode_list": ["OFFLINE"],
@@ -212,10 +206,15 @@ def pretty_print_experiment_parameters(
     print(tabulate(parameters, headers=["Parameter", "Value"], tablefmt="simple"))
 
 
+import datetime
+
+import datetime
+
+
 def get_set_of_experiments_name(
         preset_name: str, network_name: str, algo_mode_list: list[str],
         number_of_trips: int, add_shortcuts: bool, day_list: list[int], max_flow_allowed_list: list[int],
-        seed_list: list[int], list_of_slopes: list[float], list_of_thresholds: list[float],
+        seed_list: list[int], list_of_slopes_list: list[list[float]], list_of_thresholds_list: list[list[float]],
         staggering_cap: list[int], deadline_factor: int, optimize: bool, warm_start: bool,
         improve_warm_start: bool, local_search_callback: bool, simplify: bool) -> str:
     # Format the string based on the arguments and convert to uppercase
@@ -234,9 +233,12 @@ def get_set_of_experiments_name(
 
     def format_list_int(algo_mode_list: list[int]):
         if len(algo_mode_list) == 1:
-            return algo_mode_list[0]
+            return str(algo_mode_list[0])
         else:
             return str(algo_mode_list).replace(" ", "")
+
+    def format_nested_list(nested_list: list[list[float]]):
+        return "".join(["[" + "_".join(map(str, inner_list)) + "]" for inner_list in nested_list])
 
     # Get current date in DDMMYY format
     date_prefix = datetime.datetime.now().strftime("%d%m%y")
@@ -244,7 +246,7 @@ def get_set_of_experiments_name(
     name = (
         f"{date_prefix}_{preset_name}_{network_name}_SHORT{'YES' if add_shortcuts else 'NO'}_MF{format_list_int(max_flow_allowed_list)}"
         f"_{format_algo_mode_list(algo_mode_list)}_T{number_of_trips}_D{len(day_list)}_"
-        f"S{len(seed_list)}_{(list_of_slopes)}{(list_of_thresholds)}_"
+        f"S{len(seed_list)}_{format_nested_list(list_of_slopes_list)}_{format_nested_list(list_of_thresholds_list)}_"
         f"{format_stag_cap(staggering_cap)}_DL{deadline_factor}_"
         f"OPT{'YES' if optimize else 'NO'}_"
         f"WARM{'YES' if warm_start else 'NO'}_IWARM{'YES' if improve_warm_start else 'NO'}_"
@@ -260,7 +262,7 @@ def get_set_of_experiments_name(
     return name.upper()
 
 
-def main(preset_name: str, network_name: str, number_of_trips: int, add_shortcuts: bool):
+def main(preset_name: str, add_shortcuts: bool):
     """
     Main function to execute the setup and configuration for cluster jobs.
     """
@@ -269,11 +271,13 @@ def main(preset_name: str, network_name: str, number_of_trips: int, add_shortcut
         raise ValueError("preset name not in presets")
 
     # Instance parameters
+    network_name = PRESETS[preset_name]["network_name"]
+    number_of_trips = PRESETS[preset_name]["number_of_trips"]
     day_list = PRESETS[preset_name]["day_list"]
     max_flow_allowed_list = PRESETS[preset_name]["max_flow_allowed_list"]
     seed_list = PRESETS[preset_name]["seed_list"]
-    list_of_slopes = PRESETS[preset_name]["list_of_slopes"]
-    list_of_thresholds = PRESETS[preset_name]["list_of_thresholds"]
+    list_of_slopes_list = PRESETS[preset_name]["list_of_slopes_list"]
+    list_of_thresholds_list = PRESETS[preset_name]["list_of_thresholds_list"]
     staggering_cap_list = PRESETS[preset_name]["staggering_cap_list"]
     deadline_factor = PRESETS[preset_name]["deadline_factor"]
 
@@ -292,8 +296,8 @@ def main(preset_name: str, network_name: str, number_of_trips: int, add_shortcut
 
     set_of_experiments = get_set_of_experiments_name(preset_name, network_name, algo_mode_list,
                                                      number_of_trips, add_shortcuts, day_list, max_flow_allowed_list,
-                                                     seed_list, list_of_slopes, list_of_thresholds, staggering_cap_list,
-                                                     deadline_factor, optimize, warm_start,
+                                                     seed_list, list_of_slopes_list, list_of_thresholds_list,
+                                                     staggering_cap_list, deadline_factor, optimize, warm_start,
                                                      improve_warm_start, local_search_callback, simplify)
 
     # Cluster parameters
@@ -302,10 +306,10 @@ def main(preset_name: str, network_name: str, number_of_trips: int, add_shortcut
     cpu_per_run = 1
     node_type = "ANY"
     if network_name == "manhattan_5":
-        minutes_per_run = 30
+        minutes_per_run = 5
         memory_per_cpu = "2G"
     elif network_name == "manhattan_10":
-        minutes_per_run = 60
+        minutes_per_run = 15
         memory_per_cpu = "500MB"
     else:
         # LARGE EXPERIMENTS
@@ -329,22 +333,23 @@ def main(preset_name: str, network_name: str, number_of_trips: int, add_shortcut
         for seed in seed_list:
             for staggering_cap in staggering_cap_list:
                 for max_flow_allowed in max_flow_allowed_list:
-                    # Create a dictionary of parameters for the current configuration
-                    instance_params_dict = {
-                        "network_name": network_name,
-                        "day": day,
-                        "number_of_trips": number_of_trips,
-                        "seed": seed,
-                        "max_flow_allowed": max_flow_allowed,
-                        "add_shortcuts": add_shortcuts,
-                        "list_of_slopes": list_of_slopes,
-                        "list_of_thresholds": list_of_thresholds,
-                        "staggering_cap": staggering_cap,
-                        "deadline_factor": deadline_factor
-                    }
-                    instance_params_name = get_csv_data_name(instance_params_dict)
-                    write_instance_parameters_csv(instance_params_dict, instance_params_name, mode="instance")
-                    instance_params_names_list.append(instance_params_name)
+                    for pwl_id in range(len(list_of_slopes_list)):
+                        # Create a dictionary of parameters for the current configuration
+                        instance_params_dict = {
+                            "network_name": network_name,
+                            "day": day,
+                            "number_of_trips": number_of_trips,
+                            "seed": seed,
+                            "max_flow_allowed": max_flow_allowed,
+                            "add_shortcuts": add_shortcuts,
+                            "list_of_slopes": list_of_slopes_list[pwl_id],
+                            "list_of_thresholds": list_of_thresholds_list[pwl_id],
+                            "staggering_cap": staggering_cap,
+                            "deadline_factor": deadline_factor
+                        }
+                        instance_params_name = get_csv_data_name(instance_params_dict)
+                        write_instance_parameters_csv(instance_params_dict, instance_params_name, mode="instance")
+                        instance_params_names_list.append(instance_params_name)
 
     # Define solver parameters for the simulation
     solver_params_list = []
@@ -371,8 +376,9 @@ def main(preset_name: str, network_name: str, number_of_trips: int, add_shortcut
 
 
 if __name__ == "__main__":
-    main(preset_name="algo_performance_paper", network_name="manhattan_100", number_of_trips=5000,
-         add_shortcuts=True)
+    main(preset_name="var_pwl", add_shortcuts=True)
 
+# PRESETS NAMES
+# var_pwl
 # algo_performance_paper
 # staggering_analysis_paper
