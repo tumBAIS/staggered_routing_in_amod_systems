c360d00d9c80ce09239608af3a2b98002522b495
antonio.coppola <antonio.coppola@tum.de>
2024-12-26 13:24:20 +0100

    261224_VAR_PWL_MAN10_SHORTYES_MF

--- src/input_data.py
+++ src/input_data.py
@@ -126,8 +126,8 @@ def print_parameters(instance_parameters, solver_parameters):
 
 def generate_input_data_from_script() -> tuple[InstanceParameters, SolverParameters]:
     instance_params = InstanceParameters(
-        day=5, number_of_trips=500, seed=0, network_name="manhattan_10",
-        max_flow_allowed=100, add_shortcuts=True, list_of_slopes=[0.15], list_of_thresholds=[1],
+        day=5, number_of_trips=150, seed=0, network_name="manhattan_10",
+        max_flow_allowed=100, add_shortcuts=True, list_of_slopes=[0.15, 0.3], list_of_thresholds=[1, 1.5],
         deadline_factor=100, staggering_cap=25)
 
     solver_params = SolverParameters(epoch_size=60, epoch_time_limit=60,
